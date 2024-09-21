40e123663d360679194762915c8cecb62a01208a
PandaDT <151855068+PandaLehre@users.noreply.github.com>
2024-09-21 14:45:46 +0200

    updated preview pipeline

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -9,7 +9,7 @@ output_image_path = 'preview.png'
 poppler_path = r'C:\Program Files\poppler\Library\bin'
 
 # Die erste Seite der PDF in ein Bild umwandeln
-pages = convert_from_path(pdf_path, 300, first_page=1, last_page=1, poppler_path=poppler_path)
+pages = convert_from_path(pdf_path, 300, first_page=0, last_page=1, poppler_path=poppler_path)
 
 # Die erste Seite als Bild speichern
 pages[0].save(output_image_path, 'PNG')
